d19d027efa0db0150474a8a4f178c048e49efcb3
Ammon Jeffery <savingphace@gmail.com>
2020-10-10 20:50:26 -0600

    Updates

---
+++ precache-manifest.ffef905fb10b7aaaf795a1aa5eade2f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e381ace5ee1988cd60b92f55a74ce5d",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "ba8c33c380abcffefd69",
+    "url": "/React-Employee-Directory/static/css/main.a8cb7c7b.chunk.css"
+  },
+  {
+    "revision": "f455344dc7a859c30eff",
+    "url": "/React-Employee-Directory/static/js/2.deff3662.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Employee-Directory/static/js/2.deff3662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba8c33c380abcffefd69",
+    "url": "/React-Employee-Directory/static/js/main.53389d14.chunk.js"
+  },
+  {
+    "revision": "7a3b04eb39eeeb27bc59",
+    "url": "/React-Employee-Directory/static/js/runtime-main.a0389cb5.js"
+  }
+]);
